196dfc108f66e55ada18fe2962e854c410904eb3
adhulip <rudysback@gmail.com>
2016-03-24 22:06:39 -0400

    finish migration to hexo of current site; remove es6

--- themes/betweenism/source/js/src/main.js
+++ themes/betweenism/source/js/src/main.js
@@ -1,7 +1,39 @@
-import $ from "jquery";
-import {ps} from "./ps/output";
+console.log("hi there");
 
 $(function() {
-  console.log("hi");
-  console.log(ps);
+    initColorPickerInfo();
+    initColorPickerCanvas();
+    console.log(PS)
 });
+
+var initColorPickerInfo = function(){
+  updateColorPickerInfoContainer("welcome");
+
+  $(".color-picker-info-container").on('click', "a", function(e){
+    e.preventDefault();
+    var partialName = $(this).attr('href').replace(/[#]/, "");  
+    updateColorPickerInfoContainer(partialName);  
+  });
+
+  function updateColorPickerInfoContainer(partialName) {
+    var content = $("#color-picker-"+partialName).html();
+    $('.color-picker-info-container').html(content);
+  }
+}
+
+var initColorPickerCanvas = function() {
+  $('.color-picker-canvas-container').click(function(e) {
+    var posX = e.pageX - $(this).offset().left
+    var posY = e.pageY - $(this).offset().top;
+    updateColorPickerPointer(posX, posY);
+    console.log({x: posX, y: posY});
+    var currentPoint = PS.Main.findNearestPoint({x: posX, y: posY})([{x: 4, y: 5}, {x: 1, y: 2}, {x: 3, y: 6}]).value0
+    console.log(currentPoint);
+  });
+
+  function updateColorPickerPointer(posX, posY) {
+    $('.color-picker-canvas-container .sat-grad').html("<span class='color-picker-pointer'>&#9737</span>")
+    $(".color-picker-pointer").css("top", posY - ($(".color-picker-pointer").height()/2));
+    $(".color-picker-pointer").css("left", posX - ($(".color-picker-pointer").width()/2));
+  }
+}
