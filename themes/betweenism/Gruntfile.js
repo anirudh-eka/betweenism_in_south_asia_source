3ceea2cc045b24cdc8035ce6ca02313228ac7299
adhulip <rudysback@gmail.com>
2016-03-24 22:14:16 -0400

    ammend

--- themes/betweenism/Gruntfile.js
+++ themes/betweenism/Gruntfile.js
@@ -2,24 +2,27 @@ module.exports = grunt => {
   require("load-grunt-tasks")(grunt); // npm install --save-dev load-grunt-tasks
 
   grunt.initConfig({
+    uglify: {
+      js: {
+        files: { 'source/js/dist/output.min.js': ['source/js/src/purs/pursout.js','source/js/src/*.js'] },
+        options: {
+          preserveComments: false
+        }
+      }
+    },
     browserify: {
       dist: {
-        options: {
-           transform: [
-             ["babelify"]
-           ]
-        },
         files: {
-          "source/js/dist/app.js": "source/js/src/main.js"
+          "source/js/dist/app.js": "source/js/src/ps/main.js"
         }
       }
     },
     shell: {
       pulp: {
-        command: "cd source/js/src/ps/ && pulp build -O --to output.js && cd ../../../.."
+        command: "cd source/js/src/purs/ && pulp build -O --to ./pursout.js && cd ../../../.."
       }
     }
   });
   grunt.registerTask("pulp", ["shell:pulp"])
-  grunt.registerTask("default", ["pulp", "browserify"]);
+  grunt.registerTask("default", ["pulp", "uglify"]);
 }
