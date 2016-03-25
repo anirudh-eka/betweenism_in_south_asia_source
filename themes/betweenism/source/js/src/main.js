3a864a41752d83c8a949ad71588770d67b6f0c52
adhulip <rudysback@gmail.com>
2016-03-25 12:53:32 -0400

    make color picker draggable

--- themes/betweenism/source/js/src/main.js
+++ themes/betweenism/source/js/src/main.js
@@ -20,23 +20,37 @@ var initColorPickerInfo = function(){
 }
 
 var initColorPickerCanvas = function() {
-  $('.color-picker-canvas-container').click(function(e) {
-    var posX = e.pageX - $(this).offset().left
-    var posY = e.pageY - $(this).offset().top;
+  $('.color-picker-canvas-container').mousedown(function(e) {
+    handleCanvasDrag(e);
+    $(this).mousemove(function(e){
+      handleCanvasDrag(e);
+    });
+  });
+
+  $('.color-picker-canvas-container').mouseup(function(e) {
+    $(this).off("mousemove");
+  });
+
+  function handleCanvasDrag(e) {
+    var $canvas = $(e.currentTarget);
+    var posX = e.pageX - $canvas.offset().left
+    var posY = e.pageY - $canvas.offset().top;
     if(!pointIsOutsideOfCanvas(posX,posY)) {
-      updateColorPickerPointer(posX, posY);
+      updatePointerIcon(posX, posY);
+    } else {
+      $canvas.off("mousemove");
     }
     console.log({x: posX, y: posY});
     var currentPoint = PS.Main.findNearestPoint({x: posX, y: posY})([{x: 4, y: 5}, {x: 1, y: 2}, {x: 3, y: 6}]).value0
     console.log(currentPoint);
-  });
-
+  }
+  
   function pointIsOutsideOfCanvas(x,y) {
     var $canvas = $('.color-picker-canvas-container');
     return x > $canvas.width() || x < 0 || y > $canvas.height() || y < 0;
   }
 
-  function updateColorPickerPointer(posX, posY) {
+  function updatePointerIcon(posX, posY) {
     $('.color-picker-canvas-container .sat-grad').html("<span class='color-picker-pointer'>&#9737</span>")
     $(".color-picker-pointer").css("top", posY - ($(".color-picker-pointer").height()/2));
     $(".color-picker-pointer").css("left", posX - ($(".color-picker-pointer").width()/2));
